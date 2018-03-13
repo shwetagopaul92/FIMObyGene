d415830a2ca7a1c131b2566c27e991e0a7b6e5b1
shwetagopaul <mail@shwetagopaul92.com>
2018-03-13 00:08:22 -0400

    some changes

---
+++ R/runFIMObyGene.R
@@ -0,0 +1,9 @@
+######## Shiny APP ########
+### FIMObyGene ###
+
+#'shiny interface to Kimbie's CISBP FIMO scan TF bed files
+#'@import shiny
+#'@export
+runFIMObyGene<-function(){
+  runApp("inst/shinyApps/")
+}
