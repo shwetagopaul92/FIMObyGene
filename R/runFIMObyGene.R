68d4a863f6a13f1a15b8e9196400e8b28438d7e2
shwetagopaul <mail@shwetagopaul92.com>
2018-03-16 16:00:33 -0400

    change

--- R/runFIMObyGene.R
+++ R/runFIMObyGene.R
@@ -5,7 +5,7 @@
 #'@import shiny
 #'@param host character string with inet ip address
 #'@export
-runFIMObyGene<-function(ip){
+runFIMObyGene<-function(host){
   myapp = system.file("shinyApps",package="FIMObyGene")
   runApp(myapp, host=host)
 }
